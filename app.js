f64155ce8575d8fbe5de3a9f3507f8ce53552762
Spencer Snyder <sasnyde2@gmail.com>
2017-09-09 01:13:15 -0400

    added 404 and 500 error handling

--- app.js
+++ app.js
@@ -18,4 +18,19 @@ app.get('/', (req, res, next) => {
   res.send('Hello FCC!');
 });
 
+// catch 404 errors
+app.use((req, res, next) => {
+  const err = new Error();
+  err.status = 404;
+  err.message = "four oh four! We couldn't find what you were looking for";
+  res.send(err);
+});
+
+// handle server errors
+app.use((err, req, res, next) => {
+  err.status = err.status || 500;
+  console.error(err.stack);
+  res.send(err);
+});
+
 app.listen(3000, console.log('listening on port 3000!'));
