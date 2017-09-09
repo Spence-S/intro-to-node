d15dfb487ea291d83201b85204651e2402d296ed
Spencer Snyder <sasnyde2@gmail.com>
2017-09-09 00:53:44 -0400

    added morgan logger and body parser middleware

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+// middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// static files
 app.use('/html', express.static(path.join(__dirname, 'public')));
 
+// routes
 app.get('/', (req, res, next) => {
   res.send('Hello FCC!');
 });
